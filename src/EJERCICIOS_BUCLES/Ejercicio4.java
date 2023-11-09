4fb2f6534c870a2228e6d74b08ade47efc25ddc9
RubenGBar <ruben.garcia@iesnervion.es>
2023-11-09 13:55:22 +0100

    3 sin terminar y 4

---
+++ src/EJERCICIOS_BUCLES/Ejercicio4.java
@@ -0,0 +1,18 @@
+package EJERCICIOS_BUCLES;
+
+/*
+ * Valor introducido | Resultado esperado | Resultado obtenido 
+ *  				 |  				  | 		
+ * 				     |  				  |  		
+ *   				 | 					  | 		
+ *  				 |   		 		  |  		
+ *   				 |   				  |  		
+ *   				 |   				  |  		
+ * */
+
+public class Ejercicio4 {
+	public static void main(String[] args) {
+		
+	}
+
+}
