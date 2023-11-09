36906f4f0e6a77370144a8c6c9836294af05bd19
ruben <ruben@DESKTOP-QTJ799Q>
2023-11-09 22:03:45 +0100

    ej 5 sin pruebas y ej 6

---
+++ src/EJERCICIOS_BUCLES/Ejercicio5.java
@@ -0,0 +1,83 @@
+package EJERCICIOS_BUCLES;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+ * Valor introducido | Resultado esperado | Resultado obtenido 
+ *  				 |  				  | 		
+ * 				     |  				  |  		
+ *   				 | 					  | 		
+ *  				 |   		 		  |  		
+ *   				 |   				  |  		
+ *   				 |   				  |  		
+ * */
+
+public class Ejercicio5 {
+public static void main(String[] args) {
+		
+		int a = 0, b = 0, i, multiploA, multiploB, mcm = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+		
+		System.out.println("Introduzca un primer numero para calcular el m.c.m.: ");
+		
+		try { 
+			
+			a = sc.nextInt();
+			
+		}catch (InputMismatchException e) {
+			
+			a = 1;
+
+			System.out.println("Ha introducido un valor erroneo. Se introducira 1 como valor predeterminado");
+			
+		}
+		
+		System.out.println("Introduzca un segundo numero para calcular el m.c.m.: ");
+		
+		try { 
+			
+			b = sc.nextInt();
+			
+		}catch (InputMismatchException e) {
+			
+			b = 2;
+
+			System.out.println("Ha introducido un valor erroneo. Se introducira 2 como valor predeterminado");
+			
+		}
+		
+		if (a > b) System.out.println("El primer numero debe de ser menor que el segundo. ");;
+		
+		} while (a > b);
+		
+		
+		
+		
+		for (i = b + 1; i > b; i++) {
+			
+			multiploA = i % a;
+			
+			multiploB = i % b;
+			
+			
+			if (multiploA == multiploB) {
+				
+				mcm = i;
+				
+				break;
+				
+			}
+			
+		}
+		
+		System.out.println("El m.c.m. de " + a + " y " + b + " es: " + mcm);
+		
+		sc.close();
+		
+	}
+
+}
---
+++ src/EJERCICIOS_BUCLES/Ejercicio6.java
@@ -0,0 +1,20 @@
+package EJERCICIOS_BUCLES;
+
+/*
+ * Valor introducido | Resultado esperado | Resultado obtenido 
+ *  				 |  				  | 		
+ * 				     |  				  |  		
+ *   				 | 					  | 		
+ *  				 |   		 		  |  		
+ *   				 |   				  |  		
+ *   				 |   				  |  		
+ * */
+
+public class Ejercicio6 {
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
