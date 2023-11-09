e4fb4d1c2a29fe58d662396b511222bc4ba5dc7d
RubenGBar <ruben.garcia@iesnervion.es>
2023-11-09 13:54:45 +0100

    2 y 3 terminado 4 sin terminar

--- src/EJERCICIOS_BUCLES/Ejercicio2.java
+++ src/EJERCICIOS_BUCLES/Ejercicio2.java
@@ -4,58 +4,65 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /*
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido:
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
+ * Valor introducido | Resultado esperado | Resultado obtenido 
+ *  		25		 |  		9		  | 		9
+ * 			1	     |  		0		  |  		0
+ *   		0		 | Valor erroneo	  | Valor erroeno		
+ *  		a		 |   		0 		  |  		0 		
  * */
 
 public class Ejercicio2 {
 	public static void main(String[] args) {
-		
-		int numero = 0;
-		
+
+		int numero = 0, i, j, contador = 0;
+
 		boolean primo;
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Introduzca un número positivo: ");
-		
+
 		do {
-		
-		try {
-		
-			numero = sc.nextInt();
 			
-		}catch (InputMismatchException e) {
+			System.out.println("Introduzca un numero positivo: ");
 			
-			numero = 1;
+			try {
+
+				numero = sc.nextInt();
+
+			} catch (InputMismatchException e) {
+
+				numero = 1;
+
+				System.out.println("Ha introducido un valor erroneo. Se introducira 1 como valor predeterminado");
+
+			}
 			
-			System.out.println("Ha introducido un valor erroneo. Se introducira 1 como valor predeterminado");
+			if (numero < 1) System.out.println("Valor erroneo");;
 			
+		} while (numero < 1);
+
+		for (i = 2; i <= numero; i++) {
+
+			primo = true;
+
+			for (j = 2; j < i; j++) {
+				
+				if (i % j == 0) {
+					primo = false;
+					break;
+				}
+				
+			}
+
+			if (primo)
+				
+				contador++;
+
 		}
-		
-		}while(numero < 1);
-		
-		if (numero == 1) {
-			
-			System.out.println("El numero no es primo.");
-			
-		}else {
-			
-			
-			
-		}
-		
+
+		System.out.println("Entre 1 y " + numero + " hay " + contador + " numero/s primo/s");
+
 		sc.close();
-		
+
 	}
 
 }
--- src/EJERCICIOS_BUCLES/Ejercicio3.java
+++ src/EJERCICIOS_BUCLES/Ejercicio3.java
@@ -1,19 +1,58 @@
 package EJERCICIOS_BUCLES;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /*
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido:
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
- * Valor introducido es =  | Resultado esperado:  | Resultado obtenido: 
+ * Valor introducido | Resultado esperado | Resultado obtenido 
+ *  				 |  				  | 		
+ * 				     |  				  |  		
+ *   				 | 					  | 		
+ *  				 |   		 		  |  		
+ *   				 |   				  |  		
+ *   				 |   				  |  		
  * */
 
 public class Ejercicio3 {
+	public static void main(String[] args) {
+		
+		int numero;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+			
+			System.out.println("Introduzca un numero positivo: ");
+			
+			try {
+
+				numero = sc.nextInt();
+
+			} catch (InputMismatchException e) {
+
+				numero = 1;
+
+				System.out.println("Ha introducido un valor erroneo. Se introducira 1 como valor predeterminado");
+
+			}
+			
+			if (numero < 1) System.out.println("Valor erroneo");;
+			
+		} while (numero < 1);
+		
+		for (int i = 0; i <= numero; i++) {
+			
+			
+			for (int j = 1; j <= i; j++) {
+				
+				System.out.print("*");
+				System.out.print("");
+			}
+			System.out.println("");
+		}
+		
+		sc.close();
+		
+	}
 
 }
